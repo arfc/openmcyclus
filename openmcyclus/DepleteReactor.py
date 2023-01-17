de729df371815771f64d24816bbb66af0219273e
abachma2 <amandab7@illinois.edu>
2023-01-17 11:08:22 -0600

    add more State Vars and methods, importing from parent

--- openmcyclus/DepleteReactor.py
+++ openmcyclus/DepleteReactor.py
@@ -13,6 +13,7 @@ class DepleteReactor(Facility):
         tooltip="Name of commodity requested",
         uilabel="Input Commodity",
         #uitype="incommodity",
+        #alias = ['fuel_incommods']
     )
 
     fuel_inrecipes = ts.String(
@@ -35,8 +36,8 @@ class DepleteReactor(Facility):
     )
 
     assem_size = ts.Double(
-        doc = "Size of a single fuel assembly",
-        tooltip="Size of a single fuel assembly",
+        doc = "Mass (kg) of a single fuel assembly",
+        tooltip="Mass (kg) of a single fuel assembly",
         uilabel="Assembly Size",
         #uitype='assemsize',
         units="kg"
@@ -55,10 +56,61 @@ class DepleteReactor(Facility):
         tooltip="Time steps for refueling",
         uilabel="refueltime"
     )
+
+    n_assem_core = ts.Int(
+        doc = "Number of assemblies in a core",
+        tooltip = "Number of assemblies in a core",
+        uilabel = "n_assem_core"
+    )
+
+    n_assem_batch = ts.Int( 
+        doc = "Number of assemblies per batch",
+        tooltip = "Number of assemblies per batch",
+        uilabel = "n_assem_batch"
+    )
+
+    power_cap = ts.Double(
+        doc = "Maximum amount of power (MWe) produced",
+        tooltip = "Maximum amount of power (MWe) produced",
+        uilabel = "power_cap",
+        units = "MW"
+    )
     
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, ctx):
+        super().__init__(ctx)
 
-    def tick(self):
-        print("Hello World!")
+    def enter_notify(self):
+        super().enter_notify()
 
+    def check_decommission_condition(self):
+        super().check_decommission_condition()
+
+    def get_material_requests(self):
+        super().get_material_requests()
+
+    def get_material_trades(self, trades):
+        super().get_material_trades(trades)
+
+    def get_material_bids(self, requests):
+        super().get_material_bids(requests)
+
+    def accept_material_trades(self, responses):
+        super().accept_material_trades(responses)
+
+    def get_product_trades(self, trades):
+        super().get_product_trades(trades)
+
+    def get_product_bids(self, requests):
+        super().get_product_bids(requests)
+
+    def get_product_requests(self):
+        super().get_product_requests()
+
+    def accept_product_trades(self, trades):
+        super().accept_product_trades(trades)
+
+    def decision(self):
+        super().decision()
+
+    def tick():
+        print("Iteration 1")
