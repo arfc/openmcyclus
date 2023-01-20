bd1401d57c9e06e721f2b7557281ab224d5c120d
abachma2 <amandab7@illinois.edu>
2023-01-20 15:02:24 -0600

    update docstring for class

--- openmcyclus/DepleteReactor.py
+++ openmcyclus/DepleteReactor.py
@@ -2,7 +2,11 @@ from cyclus.agents import Facility
 
 class DepleteReactor(Facility):
     '''
-    Sample facility to get things going
+    Archetype class to model a reactor facility that is 
+    coupled to the stand alone depletion solver in OpenMC.
+    With the exception of the depletion solver, this 
+    archetype has the same functionality as the 
+    cycamore:Reactor archetype.
     '''
     def tick(self):
         print("Hello ")
