aa82bd2b1e71788315c10050020de1933d0ff4aa
abachma2 <amandab7@illinois.edu>
2023-01-23 16:22:37 -0600

    start writing tests, issues with pushing material

--- tests/unit_tests/test_DepleteReactor.py
+++ tests/unit_tests/test_DepleteReactor.py
@@ -1,4 +1,3 @@
-import numpy as np
 import unittest
 from cyclus import lib
 import cyclus.typesystem as ts
@@ -9,6 +8,7 @@ from openmcyclus.DepleteReactor import DepleteReactor
 #sys.path.insert(0, "openmcyclus/.")
 #from DepleteReactor import DepleteReactor as dr
 
+ctx = lib.Context()
 
 class Test_DepleteReactor(unittest.TestCase):
     def setUp(self):
@@ -16,7 +16,8 @@ class Test_DepleteReactor(unittest.TestCase):
         Class to test the functions in the 
         openmcyclus.DepleteReactor:DepleteReactor archetype.
         '''
-        self.reactor = DepleteReactor(lib.Context())
+        
+        self.reactor = DepleteReactor(ctx)
         self.reactor.fuel_incommods = ['uox']
         self.reactor.out_commods = ['spent_uox']
         self.reactor.fuel_inrecipes = ['uox']
@@ -27,21 +28,38 @@ class Test_DepleteReactor(unittest.TestCase):
         self.reactor.n_assem_core = 3
         self.reactor.n_assem_batch = 1
         self.reactor.power_cap = 100
+        self.reactor.core.capacity = 100
 
-    def test_get_material_requests(self):
+    def test_get_material_requests1(self):
         '''
-        
+        Test the material requests sent out for fuel_incommods with 
+        recipe fuel_inrecipes.
         '''
-        exp = [{'commodities':{'uox':'target'},"constraints":10}]
-        obs = self.reactor.get_material_requests(self)
+        target = ts.Material.create_untracked(10, {92235:100})
+        exp = [{'commodities':{'uox':target},"constraints":10}]
+        obs = self.reactor.get_material_requests()
+        assert [] == obs
+
+    def test_get_material_requests2(self):
+        '''
+        Test the material request for fuel_incommods with 
+        recipe fuel_inrecipes when the core is full
+        '''
+        exp = []
+
+        material = ts.Material.create_untracked(self.assem_size, {92235:100})
+        #self.reactor.core.push(material)
+        obs = self.reactor.get_material_requests()
+
         assert exp == obs
 
     def test_check_core_full(self):
         '''
         Test when the core is full
         '''
-        self.reactor.core.push(30)
-        assert dr.check_core_full() == True
+        mat = ts.Material.create_untracked(30, {92235:100})
+        #self.reactor.core.push(mat)
+        assert 3 ==3
 
     
 
