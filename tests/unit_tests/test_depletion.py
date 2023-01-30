f83bcc396f578c6e75dbcd0c2fd715b679e921f0
abachma2 <amandab7@illinois.edu>
2023-01-30 17:09:43 -0600

    add test for create recipe
    The test covers that the tage for the created xml file are correct,
    no test for the text in the file

--- tests/unit_tests/test_depletion.py
+++ tests/unit_tests/test_depletion.py
@@ -1,14 +1,29 @@
 import numpy as np
 import pytest 
-import depletion
+from openmcyclus import depletion
+import xml.etree.ElementTree as ET
 
-def test_read_model2():
+def test_read_model():
     '''
-    Test for raising an error when one of the files is not 
-    found in the given path
+    Test for when the files are found
     '''
-    model = depletion.read_model("./")
+    model = depletion.read_model("../")
     assert 2 == 2
 
 def test_read_microxs():
+    microxs = depletion.read_microxs("../")
     assert 2 == 2
+
+def test_create_recipe():
+    depletion.create_recipe("../", "uox", "./")
+    output_recipe = "./uox.xml"
+    tree = ET.parse(output_recipe)
+    root = tree.getroot()
+    assert root.tag == 'recipes'
+    assert root[0].tag == 'recipe'
+    assert root[0][0].tag == 'name'
+    assert root[0][1].tag == 'basis'
+    assert root[0][2].tag == 'nuclide'
+    assert root[0][2][0].tag == 'id'
+    assert root[0][2][1].tag == 'comp'
+    
