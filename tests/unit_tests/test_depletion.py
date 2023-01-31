36b871f591fb9bd638e887c0359f269216aaf99f
abachma2 <amandab7@illinois.edu>
2023-01-31 11:04:47 -0600

    change structure to use classes

--- tests/unit_tests/test_depletion.py
+++ tests/unit_tests/test_depletion.py
@@ -1,29 +1,37 @@
 import numpy as np
 import pytest 
-from openmcyclus import depletion
+from openmcyclus.depletion import OpenMCDepletion
 import xml.etree.ElementTree as ET
+import unittest
 
-def test_read_model():
-    '''
-    Test for when the files are found
-    '''
-    model = depletion.read_model("../")
-    assert 2 == 2
+class TestOpenMCDepletion(unittest.TestCase):
+    def setUp(self):
+        '''
+        Set up the instantiation of the OpenMCDeplete class
+        '''
+        self.deplete = OpenMCDepletion("../", 'Reactor', "chain_endfb71_pwr.xml", 10, 100)
 
-def test_read_microxs():
-    microxs = depletion.read_microxs("../")
-    assert 2 == 2
+    def test_read_model(self):
+        '''
+        Test for when the files are found
+        '''
+        model = self.deplete.read_model()
+        assert 2 == 2
 
-def test_create_recipe():
-    depletion.create_recipe("../", "uox", "./")
-    output_recipe = "./uox.xml"
-    tree = ET.parse(output_recipe)
-    root = tree.getroot()
-    assert root.tag == 'recipes'
-    assert root[0].tag == 'recipe'
-    assert root[0][0].tag == 'name'
-    assert root[0][1].tag == 'basis'
-    assert root[0][2].tag == 'nuclide'
-    assert root[0][2][0].tag == 'id'
-    assert root[0][2][1].tag == 'comp'
+    def test_read_microxs(self):
+        microxs = self.deplete.read_microxs()
+        assert 2 == 2
+
+    def test_create_recipe(self):
+        self.deplete.create_recipe()
+        output_recipe = "../Reactor_fuel.xml"
+        tree = ET.parse(output_recipe)
+        root = tree.getroot()
+        assert root.tag == 'recipes'
+        assert root[0].tag == 'recipe'
+        assert root[0][0].tag == 'name'
+        assert root[0][1].tag == 'basis'
+        assert root[0][2].tag == 'nuclide'
+        assert root[0][2][0].tag == 'id'
+        assert root[0][2][1].tag == 'comp'
     
