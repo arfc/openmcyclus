6ead5b08d2c010fcc3df3a7ed43d81f8e3bcde7f
abachma2 <amandab7@illinois.edu>
2023-02-14 14:30:25 -0600

    add second test suite
    Add a second test suite for testing functionality in a more
    complex simulation, defined by examples/complex.xml

--- tests/integration_tests/test_depletereactor.py
+++ tests/integration_tests/test_depletereactor.py
@@ -21,19 +21,15 @@ from tests.helper import check_cmd, run_cyclus, table_exist, cyclus_has_coin
 class TestDepleteReactor(unittest.TestCase):
     '''
     Test the capabilities of the DepleteReactor in a 
-    simulation. The simulation lasts 10 time steps, 
-    the reactor is an initial facility and has an exit time of 
-    -1. The reactor has 3 assemblies per core, 1 assembly per batch, 
-    cycle time of 3 time steps, refueling time of 1 time step. 
+    simulation. 
     ''' 
 
     def setUp(self):
         self.ext = ".sqlite"
-        #self.output_file = None
-        #self.input_file = None
 
         os.system('rm ' + self.output_file)
         run_cyclus("cyclus", os.getcwd(), self.input_file, self.output_file)
+        
         # Get specific tables and columns
         self.conn = sqlite3.connect(self.output_file)
         self.conn.row_factory = sqlite3.Row
@@ -74,6 +70,13 @@ class TestDepleteReactor(unittest.TestCase):
             return a[k]
 
 class TestSimple(TestDepleteReactor):
+    '''This class tests the results of a simple simulation. 
+    The simulation lasts 10 time steps, 
+    the reactor is an initial facility and has an exit time of 
+    -1 (doesn't retire during the simulation). The reactor has 3 
+    assemblies per core, 1 assembly per batch, 
+    cycle time of 3 time steps, refueling time of 1 time step. 
+    '''
     def setUp(self):
         self.input_file = "examples/simple.xml"
         self.output_file = "simple_integration.sqlite"
@@ -96,10 +99,11 @@ class TestSimple(TestDepleteReactor):
         tbl = self.transactions
         commodities = self.to_ary(tbl, "Commodity")
         times = self.to_ary(tbl,"Time")
+        unique, counts = np.unique(commodities, return_counts=True)
+        count_dict = dict(zip(unique,counts))
         assert len(tbl) == 9
-        assert all(commodities == ['uox','uox','uox','uox',
-                                   'spent_uox','uox','spent_uox','uox', 
-                                   'spent_uox'])
+        assert count_dict['uox'] == 6
+        assert count_dict['spent_uox'] == 3
         assert all(times == [0,0,0,2,2,5,5,8,8])
 
     def test_resources(self):
@@ -108,4 +112,50 @@ class TestSimple(TestDepleteReactor):
         quantities = self.to_ary(tbl, "Quantity")
         assert len(tbl) == 6
         assert all(times == [0,0,0,2,5,8])
+        assert all(quantities == [10]*6)
+
+class TestComplex(TestDepleteReactor):
+    '''This class tests the results of a more 
+    complex simulation. 
+    The simulation lasts 20 time steps, 
+    the reactor is deployed at time step 3 and has a lifetime 
+    of 10 timesteps. The reactor has 3 
+    assemblies per core, 1 assembly per batch, 
+    cycle time of 3 time steps, refueling time of 1 time step. 
+    '''
+    def setUp(self):
+        self.input_file = "examples/complex.xml"
+        self.output_file = "complex_integration.sqlite"
+        super(TestComplex, self).setUp()
+
+    def test_agent_entry(self):
+        pass  
+        tbl = self.agent_entry
+        agent_ids = self.to_ary(self.agent_entry, "AgentId")
+        enter_time = self.to_ary(self.agent_entry, "EnterTime")
+        lifetimes = self.to_ary(self.agent_entry, "Lifetime")
+        rx_id = self.find_ids(":openmcyclus.DepleteReactor:DepleteReactor", tbl)
+        assert len(agent_ids) == 5
+        assert len(rx_id) == 1
+        assert all(enter_time == [0, 0, 0, 0, 3])
+        assert all(lifetimes == [-1, -1, -1, -1, 10])
+        #assert_equal(enter_time , 0)
+
+    def test_transactions(self):
+        tbl = self.transactions
+        commodities = self.to_ary(tbl, "Commodity")
+        times = self.to_ary(tbl,"Time")
+        unique, counts = np.unique(commodities, return_counts=True)
+        count_dict = dict(zip(unique,counts))
+        assert len(tbl) == 9
+        assert count_dict['uox'] == 6
+        assert count_dict['spent_uox'] == 3
+        assert all(times == [3,3,3,5,5,8,8,11,11])
+
+    def test_resources(self):
+        tbl = self.resources
+        times = self.to_ary(tbl, "TimeCreated")
+        quantities = self.to_ary(tbl, "Quantity")
+        assert len(tbl) == 6
+        assert all(times == [3,3,3,5,8,11])
         assert all(quantities == [10]*6)
