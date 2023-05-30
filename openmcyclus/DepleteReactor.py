0d0aa91e98bb45034987f7b54b87a7fc47fc2512
abachma2 <amandab7@illinois.edu>
2023-05-30 15:09:28 -0500

    resolve conflicts with preferences branch

--- openmcyclus/DepleteReactor.py
+++ openmcyclus/DepleteReactor.py
@@ -598,17 +598,9 @@ class DepleteReactor(Facility):
         if n == 0:
             return
         ss = str(n) + " assemblies"
-<<<<<<< HEAD
         #self.record("LOAD", ss)
         self.core.push_many(self.fresh_fuel.pop_n(n))
         return 
-=======
-        # self.record("LOAD", ss)
-        assemblies = self.fresh_fuel.pop_n(n)
-        for ii in range(len(assemblies)):
-            self.core.push(assemblies[ii])
-        return
->>>>>>> preferences
 
     def transmute(self, n_assem):
         '''
@@ -630,15 +622,10 @@ class DepleteReactor(Facility):
             Number of assemblies to be transmuted
         '''
         old = self.core.pop_n(min(n_assem, self.core.count))
-<<<<<<< HEAD
         self.core.push_many(old)
         if self.core.count > len(old):
             self.core.push_many(self.core.pop_n(self.core.count - len(old)))
 
-=======
-        for ii in range(len(old)):
-            self.core.push(old[ii])
->>>>>>> preferences
         ss = str(len(old)) + " assemblies"
         # self.record("TRANSMUTE", ss)
         print("time:", self.context.time, "transmute", ss)
@@ -738,12 +725,6 @@ class DepleteReactor(Facility):
 
     def peek_spent(self):
         '''
-<<<<<<< HEAD
-        Get the material in the spent fuel inventory. Then for each material,
-        get the index of its out commodity, and append that index to the 
-        end of a list that is a value in a dictionary, the keys are 
-        the names of each commodity
-=======
         Creates a dictionary of the materials in the spent 
         fuel inventory based on their commodity name. 
 
@@ -753,7 +734,6 @@ class DepleteReactor(Facility):
             Keys are the commodity names of the spent fuel. 
             Values are the Materials with the given commodity names. 
 
->>>>>>> preferences
         '''
         print("peek_spent")
         mapped = {}
@@ -764,13 +744,9 @@ class DepleteReactor(Facility):
             self.spent_fuel.push_many(mats)
             for ii in range(len(mats)):
                 commod = self.get_commod(mats[ii], 'out')
-<<<<<<< HEAD
                 print("get_commod", commod, mats[ii])
                 mapped[commod].append(mats[ii])
         print(mapped)
-=======
-                mapped[commod] = mats[ii]
->>>>>>> preferences
         return mapped
 
     def get_commod(self, material, flow):
